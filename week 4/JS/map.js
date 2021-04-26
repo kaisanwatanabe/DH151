518ce3ef03915c15c014edc9e5da9556d05ee2de
kwatanabe <kaiwatanabe@g.ucla.edu>
2021-04-25 23:28:37 -0700

    Ass4

--- week 4/JS/map.js
+++ week 4/JS/map.js
@@ -4,8 +4,10 @@ let lat = 0;
 let lon = 0;
 let zl = 3;
 
+
+
 // path to csv data
-let path = "CSV/dunitz.csv";
+let path = "CSV/query2015_2017.csv";
 // global variables
 let markers = L.featureGroup();
 
@@ -39,46 +41,68 @@ function readCSV(path){
 	});
 }
 
-function panToImage(index){
+function panToQuake(index){
     // zoom to level 17 first
-    map.setZoom(17);
+    map.setZoom(15);
     // pan to the marker
     map.panTo(markers.getLayers()[index]._latlng);
 }
 
 function mapCSV(data){
+	//scaling radius
+	let RMax = 15;
+	let RMin = 2;
 
-    // circle options
-	let circleOptions = {
-		radius: 5,
-		weight: 1,
-		color: 'white',
-		fillColor: 'dodgerblue',
-		fillOpacity: .75
-	}
-	
+	var column = [];
+    for(var i=0; i<data.data.length-2; i++){
+        column.push(parseFloat(data.data[i].mag));
+    }
+
+	let Max = Math.max(...column);
+	let Min = Math.min(...column);
+
+    
 	// loop through each entry
-	data.data.forEach(function(item,index){
+	
+	data.data.forEach(function(item,index){		
 		// create marker
-		let marker = L.circleMarker([item.latitude,item.longitude],circleOptions).on('mouseover',function()
-        {
-			this.bindPopup(`${item.title}<br><img src="${item.thumbnail_url}">`).openPopup()
-		})
+		if(index<=1900){
+			//calculate radius
+			let percent = (item.mag - Min) / (Max - Min);
+			let rad = percent * (RMax - RMin) + RMin;
+			// circle options
+			let circleOptions = {
+				radius: rad,
+				weight: 1,
+				color: 'white',
+				fillColor: 'dodgerblue',
+				fillOpacity: .75
+			}
+			
+			let marker = L.circleMarker([item.latitude,item.longitude],circleOptions).on('mouseover',function()
+			{
+				this.bindPopup(`<div>Location: ${item.place}</div><br><div>Time: ${item.time}</div><br><div>Magnitude: ${item.mag}</div>`).openPopup();
+			})
 
 
-		// add marker to featuregroup
-		markers.addLayer(marker)
+			// add marker to featuregroup
+			markers.addLayer(marker)
 
-        // add entry to sidebar
-		$('.sidebar').append(`<img src="${item.thumbnail_url}" onmouseover="panToImage(${index})">`)
-	})
+			// add entry to sidebar
+			$('.sidebar').append(
+				// `<img src="${item.thumbnail_url}" onmouseover="panToImage(${index})">`
+				`<div class = "sidebartitle" onclick="panToQuake(${index})"> ${item.place}<br>${item.time}</div>`
+			)
+		}
+	});
+	
 
 	// add featuregroup to map
 	markers.addTo(map)
 
 	// fit markers to map
 	map.fitBounds(markers.getBounds())
-    function panToImage(index){
+    function panToQuake(index){
 	// zoom to level 17 first
 	map.setZoom(17);
 	// pan to the marker
